42d829ea37ff0ca6e940a438d649347425b41d69
Levin Gerdes <gerdeslevin@gmail.com>
2017-09-06 20:43:48 +0200

    Initial commit by rock-create-orogen

---
+++ slippage_estimatorTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef slippage_estimator_TYPES_HPP
+#define slippage_estimator_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace slippage_estimator {
+}
+
+#endif
+
