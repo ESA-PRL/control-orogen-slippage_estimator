8c4ff7027ce39de75e9bc0006783de1ef8f9a01b
Levin Gerdes <gerdeslevin@gmail.com>
2017-09-08 17:50:47 +0200

    First draft of the slippage estimator

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -21,6 +21,11 @@ bool Task::configureHook()
 {
     if (! TaskBase::configureHook())
         return false;
+    integrationWindowSize = _integration_window.value();
+    slipRatioBuffer = std::vector<double>(integrationWindowSize,0.0);
+    bufferIndex = 0;
+    havePreviousPose = false;
+    haveMotionCommand= false;
     return true;
 }
 bool Task::startHook()
@@ -32,6 +37,43 @@ bool Task::startHook()
 void Task::updateHook()
 {
     TaskBase::updateHook();
+    if (_pose.read(pose) == RTT::NewData)
+    {
+        // in the beginning it might happen, that we do not have a motion command or previousPose
+        if (havePreviousPose && haveMotionCommand)
+        {
+            double deltaPose = calcDeltaPose(pose,previousPose);
+            double deltaTime = calcDeltaTime(pose,previousPose);
+            if (motionCommand.translation != 0.0)
+            {
+                slipRatioBuffer[bufferIndex] = deltaPose/(motionCommand.translation * deltaTime);
+                bufferIndex = (bufferIndex+1)%integrationWindowSize;
+            }
+        }
+        previousPose = pose;
+        havePreviousPose = true;
+        if (_motion_command.read(motionCommand) == RTT::NewData)
+        {
+            haveMotionCommand = true;
+        }
+    }
+
+    double slipRatio = 0.0;
+    for (int i=0; i < integrationWindowSize; i++)
+    {
+        slipRatio += slipRatioBuffer[i];
+    }
+    _slip_ratio.write(slipRatio/double(integrationWindowSize));
+}
+
+double Task::calcDeltaPose(base::samples::RigidBodyState cur, base::samples::RigidBodyState prev)
+{
+    return sqrt(pow(cur.position[0] - prev.position[0],2) + pow(cur.position[1] - prev.position[1],2) + pow(cur.position[2] - prev.position[2],2));
+}
+
+double Task::calcDeltaTime(base::samples::RigidBodyState cur, base::samples::RigidBodyState prev)
+{
+    return cur.time.toSeconds() - cur.time.toSeconds();
 }
 void Task::errorHook()
 {
--- tasks/Task.hpp
+++ tasks/Task.hpp
@@ -2,12 +2,26 @@
 #define SLIPPAGE_ESTIMATOR_TASK_TASK_HPP
 
 #include "slippage_estimator/TaskBase.hpp"
+#include <math.h>
 
 namespace slippage_estimator{
     class Task : public TaskBase
     {
 	friend class TaskBase;
     protected:
+        std::vector<double> slipRatioBuffer;
+        int integrationWindowSize;
+        int bufferIndex; // current position in the circular buffer of slip ratios
+
+        base::samples::RigidBodyState pose;
+        base::samples::RigidBodyState previousPose;
+        base::commands::Motion2D motionCommand;
+
+        double calcDeltaPose(base::samples::RigidBodyState,base::samples::RigidBodyState);
+        double calcDeltaTime(base::samples::RigidBodyState,base::samples::RigidBodyState);
+
+        bool havePreviousPose; // true iff at least two pose samples were read
+        bool haveMotionCommand; // true iff we ever read a motion command
 
     public:
         Task(std::string const& name = "slippage_estimator::Task");
