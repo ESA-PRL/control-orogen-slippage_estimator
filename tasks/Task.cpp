9c66192b30c13a29e2f304e6ddc5f4c6e27dd973
Martin Azkarate <martin.azkarate@gmail.com>
2017-09-19 12:44:45 +0200

    Check NaN positions, Slip estimation in WW and time window for Spot Turn

--- tasks/Task.cpp
+++ tasks/Task.cpp
@@ -22,7 +22,12 @@ bool Task::configureHook()
     if (!TaskBase::configureHook())
         return false;
     //integrationWindowSize = _integration_window.value();
+    //ww_velocity = _ww_velocity.value(); // [m/s] translational body velocity in WW mode
+    pose_samples_period = 0.1; // 10Hz
+    ww_velocity = 0.02;
+    locomotion_mode=DRIVING;
     integrationWindowSize = 100;
+    turnSpotWindowSize = 50;
     slipRatioBuffer.resize(integrationWindowSize);
     for (int i=0; i<integrationWindowSize;i ++)
     {
@@ -43,28 +48,67 @@ void Task::updateHook()
 {
     TaskBase::updateHook();
 
+    if (_locomotion_mode.read(locomotion_mode) == RTT::NewData)
+    {
+        std::cout << "Current Locomotion Mode is " << locomotion_mode << std::endl;
+    }
+    
     if (_pose.read(pose) == RTT::NewData)
     {
-        // in the beginning it might happen, that we do not have a motion command or previousPose
-        if (havePreviousPose && haveMotionCommand)
+        if (isPoseValid(pose))
         {
-            double deltaPose = calcDeltaPose(pose,previousPose);
-            double deltaTime = calcDeltaTime(pose,previousPose);
-            std::cout << "deltaPose: " << deltaPose << "  deltaTime: " << deltaTime << std::endl;
-            if (motionCommand.translation != 0.0)
+            // in the beginning it might happen, that we do not have a motion command or previousPose
+            if (havePreviousPose && haveMotionCommand)
             {
-                double slip = deltaPose/(fabs(motionCommand.translation) * deltaTime);
-                std::cout << " slip: " << slip <<std::endl;
-                slip = (slip > 1 ? 1 : slip);
-                slipRatioBuffer[bufferIndex] = 1-slip;
-                bufferIndex = (bufferIndex+1)%integrationWindowSize;
+                double deltaPose = calcDeltaPose(pose,previousPose);
+                double deltaTime = calcDeltaTime(pose,previousPose);
+                std::cout << "deltaPose: " << deltaPose << "  deltaTime: " << deltaTime << std::endl;
+                if (deltaTime>(pose_samples_period-0.001)) // pose samples should arrive at a certain frequency. If we get a burst of pose samples with almost zero delta time, it's better not to make slip estimations, it will be noise divided by zero.
+                {
+                    if (locomotion_mode == DRIVING)
+                    {
+                        if (motionCommand.translation != 0.0)
+                        {
+                            if (turn_spot)  // Transitioning from Spot Turn to normal driving. Reduce the counter and unset the flag if enough time is passed.
+                            {
+                                counter--;
+                                if (counter < 1)
+                                {
+                                    turn_spot = false;
+                                }                            
+                            } 
+                            else
+                            {
+                                double slip = 1 - deltaPose/(fabs(motionCommand.translation) * deltaTime);
+                                std::cout << "DR slip: " << slip << std::endl;
+                                slip = (slip < 0 ? 0 : slip);
+                                slipRatioBuffer[bufferIndex] = slip;
+                                bufferIndex = (bufferIndex+1)%integrationWindowSize;
+                            }
+                        }
+                        else if (motionCommand.rotation != 0.0)
+                        {
+                            turn_spot = true;
+                            counter = turnSpotWindowSize;
+                            // We don't calculate slip in Turn Spot manoeuvres.
+                        }
+                    }
+                    else if (locomotion_mode == WHEEL_WALKING)
+                    {
+                        double slip = 1 - deltaPose/(ww_velocity * deltaTime);
+                        std::cout << "WW slip: " << slip << std::endl;
+                        slip = (slip < 0 ? 0 : slip);
+                        slipRatioBuffer[bufferIndex] = slip;
+                        bufferIndex = (bufferIndex+1)%integrationWindowSize;
+                    }
+                }
             }
-        }
-        previousPose = pose;
-        havePreviousPose = true;
-        if (_motion_command.read(motionCommand) == RTT::NewData)
-        {
-            haveMotionCommand = true;
+            previousPose = pose;
+            havePreviousPose = true;
+            if (_motion_command.read(motionCommand) == RTT::NewData)
+            {
+                haveMotionCommand = true;
+            }
         }
     }
 
@@ -85,6 +129,13 @@ double Task::calcDeltaTime(base::samples::RigidBodyState cur, base::samples::Rig
 {
     std::cout << cur.time.toMilliseconds() << " <- cur <- time in milliseconds -> prev ->  " << prev.time.toMilliseconds() << std::endl;
     return (double(cur.time.toMilliseconds() - prev.time.toMilliseconds())/1000.0);
+}
+
+bool Task::isPoseValid(base::samples::RigidBodyState pose)
+{
+    if ( isnan(pose.position(0)) || isnan(pose.position(1)) || isnan(pose.position(2)) )
+        return false;
+    return true;
 }
 void Task::errorHook()
 {
